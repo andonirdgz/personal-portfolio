64279cc119aeb2d4880e2ac11bad4a3e6e01edd2
Andoni <andoni_rdgz@outlook.com>
2022-05-08 21:18:57 -0500

    Added home page

--- src/components/pages/home/Home.js
+++ src/components/pages/home/Home.js
@@ -1,11 +1,22 @@
 import logo from "./initials-logo.svg"
 import "./home.css"
+import { NavLink } from "react-router-dom"
 
 function Home() {
   return (
     <div className='Home'>
       <header className='Home-header'>
-        <img src={logo} className='Home-logo' alt='logo' />
+        <img src={logo} className='Home-logo' alt='Andoni Rodriguez Initials' />
+        <div class='separator'></div>
+        <nav>
+          <NavLink to={"/about"} exact='true' className={"animate label-name on-hover-40 "} />
+          <NavLink to={"/work"} exact='true' className={"animate label-lastname on-hover-40 "} />
+          <NavLink
+            to={"/contact"}
+            exact='true'
+            className={"animate label-occupation on-hover-40"}
+          />
+        </nav>
       </header>
     </div>
   )
